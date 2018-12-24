28f256d421ea7f4f2d03c58a0ecce23defabb17b
Trần Quốc Đạt <quocdat23894@gmail.com>
2018-12-24 02:37:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
 const path = require('path')
+
+
+
+console.log("HALP!");
+console.warn("HALP!");
+console.error("HALP!");
